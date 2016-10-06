7fa3e2cafec47631135d14df085e7f9b7fa9ade8
naturegirl <jennifer.jinyuan.guo@gmail.com>
2016-10-05 22:07:55 -0700

    changes to fake images generation script

--- code/fake_training_data/gen-images.py
+++ code/fake_training_data/gen-images.py
@@ -9,24 +9,40 @@
 # To test whether open cv installed successfully,
 # open a python console and run "import cv2"
 
+import argparse
 import numpy as np
 import cv2
 import random
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-s', '--image_size', type=int, default=100,
+                   help='size of each image')
+parser.add_argument('-n', '--num_images', type=int, default=16,
+                    help='total number of images in dataset')
+
+args = parser.parse_args()
+print(args.image_size)
+
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+IMAGE_FILENAME = "train.bmp"
+LABELS_FILENAME = "train.txt"
 
+# generate image, and return it together with labels
+# labels should be:
+#  - direction: left, right, straight
+#  - angle: in degrees?
 def gen_img():
-	height = 200	# image height
-	line_width = height / 3
-	img = np.zeros((height,height,3), np.uint8)
+	n = args.image_size
+	line_width = n / 3
+	img = np.zeros((n, n, 3), np.uint8)
 	theta = np.radians(random_angle())
 	print("theta", theta)
-	x = height * np.tan(theta) # distance from center
+	x = n * np.tan(theta) # distance from center
 	print("x", x)
-	line_end_x = int(height/2 + x)
+	line_end_x = int(n/2 + x)
 	print("line end x", line_end_x)
-	cv2.line(img, (height/2, height), (line_end_x,0), GREEN, line_width)
+	cv2.line(img, (n/2, n), (line_end_x,0), GREEN, line_width)
 
 	# add border
 	# width = 10
@@ -64,7 +80,9 @@ def combine_images(imgs, n_cols, n_rows):
 	combined = np.concatenate(rows, axis=0)
 	return combined
 
-N = 16 # number of images to generate
+N = args.num_images # number of images to generate
 imgs = [gen_img() for i in range(N)]
 combined = combine_images(imgs, 4, 4)
-show_img(combined)
+#show_img(combined)
+print("write to file")
+cv2.imwrite(IMAGE_FILENAME, combined)
