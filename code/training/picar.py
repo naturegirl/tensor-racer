a6e710ce5e73f876177205d97c79232a26e6e94d
naturegirl <jennifer.jinyuan.guo@gmail.com>
2016-10-05 20:31:23 -0700

    minor fixes

--- code/training/picar.py
+++ code/training/picar.py
@@ -1,4 +1,4 @@
-"""Functions for reading training data and surfacing via a Dataset class"""
+"""Functions for reading training  data and surfacing via a Dataset class"""
 
 # Example Usage:
 #    picar = read_data_sets("/Users/naturegirl/code/tensor-racer/data/round1)
@@ -15,7 +15,9 @@ import os
 from tensorflow.python.framework import dtypes
 
 ORIGINAL_SIZE = 100 # original images are 100x100
+RESIZE = 30         # images will be resized to 30x30 for training
 LABELS = {'left': 0, 'right': 1, 'straight': 2}
+VALIDATION_SIZE = 100   # size of the validation dataset
 
 class DataSet(object):
     # TODO: add boolean: greyscale?
@@ -63,10 +65,10 @@ def _load_images(data_dir):
     labels = [triple[2] for triple in files]
     num_rows = len(labels)
     images = [_read_image(os.path.join(data_dir, fpath)) for _, fpath, _ in files]
-    matrix = np.concatenate(images).reshape(num_rows, 30*30*3)
+    matrix = np.concatenate(images).reshape(num_rows, RESIZE * RESIZE * 3)
     return labels, matrix
 
-def _read_image(path, resize=True, size=30, rgb=True):
+def _read_image(path, resize=True, size=RESIZE, rgb=True):
     """read one image and return it as a one dimensional numpy array
     path: path to image:
     resize: whether the image should be resized
@@ -102,7 +104,6 @@ def read_data_sets(data_dir):
     """Use this public function to read the dataset"""
     # TODO: add shuffling parameter?
     labels, images = _load_images(data_dir)
-    VALIDATION_SIZE = 100
 
     train_labels = labels[VALIDATION_SIZE:]
     train_images = images[VALIDATION_SIZE:]
